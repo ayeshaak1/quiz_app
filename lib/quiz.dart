207aad2aa42c5a02b00a25a1ca915d4acf2e6655
Ayesha Khan <ayeshaa.k3002@gmail.com>
2024-06-24 04:15:22 -0400

    Added logic to go to StartScreen when questions end

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -23,6 +24,12 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length){
+      setState(() {
+        activeScreen = StartScreen(switchScreen);
+      });
+    }
   }
 
   void switchScreen() {
