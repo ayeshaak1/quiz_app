4d8792aaebd81e3a3bcaa161ab9376470f17cd81
Ayesha Khan <ayeshaa.k3002@gmail.com>
2024-06-24 04:57:53 -0400

    Added questions and answers dynamically in ResultsScreen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,7 +4,6 @@ import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/results_screen.dart';
 
-
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
@@ -27,9 +26,11 @@ class _QuizState extends State<Quiz> {
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
 
-    if (selectedAnswers.length == questions.length){
+    if (selectedAnswers.length == questions.length) {
       setState(() {
-        activeScreen = const ResultsScreen();
+        activeScreen = ResultsScreen(
+          chosenAnswers: selectedAnswers,
+        );
       });
     }
   }
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.chosenAnswers});
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +28,7 @@ class ResultsScreen extends StatelessWidget {
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(30),
-        child:  Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
@@ -18,7 +37,7 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answers and questions...'),
+            QuestionsSummary(getSummaryData()),
             ElevatedButton(
               onPressed: () {},
               child: const Text('Restart Quiz'),
