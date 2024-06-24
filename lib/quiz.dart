0d357c728f3b1c1897f8ce418be14454151b5285
Ayesha Khan <ayeshaa.k3002@gmail.com>
2024-06-24 04:35:00 -0400

    Added ResultsScreen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/results_screen.dart';
+
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -27,7 +29,7 @@ class _QuizState extends State<Quiz> {
 
     if (selectedAnswers.length == questions.length){
       setState(() {
-        activeScreen = StartScreen(switchScreen);
+        activeScreen = const ResultsScreen();
       });
     }
   }
