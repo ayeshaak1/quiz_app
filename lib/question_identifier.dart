b2b170bd1adb498294306db19911ffe434412928
Ayesha Khan <ayeshaa.k3002@gmail.com>
2024-06-24 18:43:33 -0400

    Added formatting structure for results screen

---
+++ lib/question_identifier.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionIdentifier extends StatelessWidget {
+  const QuestionIdentifier({
+    super.key,
+    required this.isCorrectAnswer,
+    required this.questionIndex,
+  });
+
+  final int questionIndex;
+  final bool isCorrectAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    final questionNumber = questionIndex + 1;
+
+    return Container(
+      width: 20,
+      height: 20,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: isCorrectAnswer ? Colors.green : Colors.red,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Text(
+        questionNumber.toString(),
+        style: GoogleFonts.comicNeue(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
